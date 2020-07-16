6d120418d5a727485fc3096f68862a8f2c7e55ce
Mat <uniatowski.mateusz@gmail.com>
2020-07-16 09:29:50 +0200

    Refactoring to spring boot and thymeleaf

---
+++ web/src/main/java/unig/programming/util/ViewNames.java
@@ -0,0 +1,11 @@
+package unig.programming.util;
+
+public class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames(){};
+
+}
